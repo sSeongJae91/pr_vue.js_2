83c7a6674d463def3404cad18a5e1a2e9954c5a6
sSeongjae91 <ben91429@gmail.com>
2021-01-20 23:43:13 +0900

    siginin

---
+++ src/components/SigninForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <form @submit.prevent="submit">
+        <fieldset>
+            <input type="text"
+                v-model="email"
+                placeholder="이메일을 입력해주세요."/>
+            <input type="password"
+                v-model="password"
+                placeholder="비밀번호를 입력해주세요."/>
+            <button type="submig">로그인</button>
+        </fieldset>
+    </form>
+</template>
+<script>
+export default {
+    name: 'SigninForm',
+    data () {
+        return {
+            email: '',
+            password: ''
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
---
+++ src/pages/Signin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>로그인</h3>
+        <p>회원이 아니신가요?
+            <router-link :to="{name: 'Signup'}">회원가입하러 가기</router-link>
+        </p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Signin'
+}
+</script>
+<style>
+    
+</style>
