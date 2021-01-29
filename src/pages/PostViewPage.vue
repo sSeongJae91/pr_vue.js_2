8923bcad9ea529e66b888bafe68338d7a8cde77b
sSeongjae91 <ben91429@gmail.com>
2021-01-30 00:38:36 +0900

    post delete

--- src/pages/PostViewPage.vue
+++ src/pages/PostViewPage.vue
@@ -3,18 +3,37 @@
         <post-view v-if="post" :post="post"/>
         <p v-else>게시글 불러오는 중...</p>
         <router-link :to="{name: 'PostEditPage', params: {postId}}">수정</router-link>
+        <button @click="onDelete">삭제</button>
         <router-link :to="{name: 'PostListPage'}">목록</router-link>
     </div>
 </template>
 <script>
 import {mapActions, mapState} from 'vuex'
 import PostView from '@/components/PostView'
+import api from '@/api'
 
 export default {
     name: 'PostViewPage',
     components: { PostView },
     methods: {
-        ...mapActions(['fetchPost'])
+        ...mapActions(['fetchPost']),
+        onDelete () {
+            const {id} = this.post
+
+            api.delete(`/posts/${id}`)
+                .then(res => {
+                    alert('게시물이 성공적으로 삭제되었습니다.')
+                    this.$router.push({name: 'PostListPage'})
+                })
+                .catch(err => {
+                    if(err.response.status === 401) {
+                        alert('로그인이 필요합니다.')
+                        this.$router.push({name:'Signin'})
+                    }else {
+                        alert(err.response.data.msg)
+                    }
+                })
+        }
     },
     props: {
         postId: {
